2053600e4aca70fae21acddd6aa753f4668adf88
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-08-16 14:22:34 +1100

    fix(prepare): use env name to find promoted env name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,22 @@ printstep "Vérification des paramètres d'entrée"
 init_env
 int_gitlab_api_env
 
-printstep "Préparation du projet $PROJECT_NAMESPACE/$PROJECT_NAME"
 declare -A PROMOTIONS
 PROMOTIONS=(["DEV"]="RECSMA" ["RECSMA"]="INT" ["RECSEP"]="INT" ["INT"]="PREPROD" ["PREPROD"]="PROD") ;
 SOURCE_BRANCH=`echo "$CI_ENVIRONMENT_NAME" | tr '[:upper:]' '[:lower:]'`
-DEST_BRANCH=`echo "${PROMOTIONS[$SOURCE_BRANCH]}" | tr '[:upper:]' '[:lower:]'`
+DEST_BRANCH=`echo "${PROMOTIONS[$CI_ENVIRONMENT_NAME]}" | tr '[:upper:]' '[:lower:]'`
 
+if [[ -z $CI_ENVIRONMENT_NAME ]];then
+    printerror "La variable CI_ENVIRONMENT_NAME de l'environnement source n'est pas renseignée"
+    exit 1
+fi
+
+if [[ -z $DEST_ENV ]];then
+    printerror "L'environnement de destination pour la promotion de $CI_ENVIRONMENT_NAME est inconnu"
+    exit 1
+fi
+
+printstep "Préparation du projet $PROJECT_NAMESPACE/$PROJECT_NAME"
 PROJECT_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_NAME" | jq --arg project_namespace "$PROJECT_NAMESPACE" '.[] | select(.namespace.name == "\($project_namespace)") | .id'`
 
 GITLAB_CI_USER_MEMBERSHIP=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/members?query=$GITLAB_CI_USER" | jq .[0]`
